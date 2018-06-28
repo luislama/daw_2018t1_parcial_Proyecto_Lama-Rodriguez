f3a3310bbe99b9eeccd3339bc892f553b2ace69a
luislama <luislama@espol.edu.ec>
2018-06-28 08:36:19 -0500

    faltan corregir unos links y mejorar la presentacion

---
+++ js/grupos.js
@@ -0,0 +1,20 @@
+function grupos(){
+	let aviso = "";
+	aviso += "Grupo 6:\n";
+	aviso += " -index.html\n";
+	aviso += " -profile.html\n";
+	aviso += " -cart.html\n";
+	aviso += " -terms.html\n";
+	aviso += " -blogs.html\n";
+	aviso += "Extras:\n";
+	aviso += "-story.html\n";
+	aviso += "Las demas pantallas son desarrolladas por el grupo 3.\n";
+	alert(aviso);
+};
+
+(function(){
+	$("#linkProductos").on("click",grupos);
+	$("#linkSocios").on("click",grupos);
+	$("#linkContacto").on("click",grupos);
+	$("#linkFaqs").on("click",grupos);
+})();
